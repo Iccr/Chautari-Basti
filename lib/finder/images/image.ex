4cf680dbe256a90e9fcf6f6fa38292869aaa2572
ccr <sis.ccr@gamil.com>
2021-01-23 22:39:17 +0545

    generated Images

---
+++ lib/finder/images/image.ex
@@ -0,0 +1,17 @@
+defmodule Finder.Images.Image do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "images" do
+    field :image, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(image, attrs) do
+    image
+    |> cast(attrs, [:image])
+    |> validate_required([:image])
+  end
+end
