1569a6f29ec4f142699bced3b9a02df7e04c44b5
ccr <sis.ccr@gamil.com>
2021-02-14 17:09:01 +0545

    adding room presence

--- lib/finder_web/channels/rent_room_channel.ex
+++ lib/finder_web/channels/rent_room_channel.ex
@@ -6,7 +6,7 @@ defmodule FinderWeb.RentRoomChannel do
   @impl true
   def join("rent_room:" <> _conversation_id, payload, socket) do
     if authorized?(socket, payload) do
-      # send(self(), :after_join)
+      send(self(), :after_join)
       {:ok, socket}
     else
       {:error, %{reason: "unauthorized"}}
@@ -23,27 +23,27 @@ defmodule FinderWeb.RentRoomChannel do
     end
   end
 
-  def handle_in(
-        "user_room:presence_update",
-        %{"typing" => typing, "user_id" => user_id} = _payload,
-        socket
-      ) do
-    if Guardian.Phoenix.Socket.authenticated?(socket) do
-      IO.puts("before update")
-      IO.inspect(RoomPresence.list(socket))
+  # def handle_in(
+  #       "user_room:presence_update",
+  #       %{"typing" => typing, "user_id" => user_id} = _payload,
+  #       socket
+  #     ) do
+  #   if Guardian.Phoenix.Socket.authenticated?(socket) do
+  #     IO.puts("before update")
+  #     IO.inspect(RoomPresence.list(socket))
 
-      RoomPresence.update(socket, user_id, %{
-        user_id: user_id,
-        typing: typing
-      })
+  #     RoomPresence.update(socket, user_id, %{
+  #       user_id: user_id,
+  #       typing: typing
+  #     })
 
-      IO.puts("after update")
-      IO.inspect(RoomPresence.list(socket))
-      broadcast(socket, "presence_state", RoomPresence.list(socket))
+  #     IO.puts("after update")
+  #     IO.inspect(RoomPresence.list(socket))
+  #     broadcast(socket, "presence_state", RoomPresence.list(socket))
 
-      {:noreply, socket}
-    end
-  end
+  #     {:noreply, socket}
+  #   end
+  # end
 
   # Channels can be used in a request/response fashion
   # by sending replies to requests from the client
@@ -80,10 +80,7 @@ defmodule FinderWeb.RentRoomChannel do
         typing: false
       })
 
-    IO.puts("Broadcasting presence_state...")
-    IO.inspect(RoomPresence.list(socket))
     broadcast(socket, "presence_state", RoomPresence.list(socket))
-
     {:noreply, socket}
   end
 
